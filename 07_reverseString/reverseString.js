6bf8213599f1a636566c50b739e5b0fb2d9eacf8
Zachary Martinez <zach.martinez@protonmail.com>
2025-11-24 18:17:57 -0800

    Solve exercise 07

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let str = '';
+    const chars = string.split('');
 
+    for(let i = chars.length - 1; i >= 0; i--) {
+        str += chars[i];
+    }
+
+    return str;
 };
 
 // Do not edit below this line
