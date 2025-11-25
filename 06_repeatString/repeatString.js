476cc4be140febbaa527dacb263b42e72f0e6b46
Zachary Martinez <zach.martinez@protonmail.com>
2025-11-24 18:09:32 -0800

    Solve exercise 06

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, reps) {
+    let newString = '';
+    if (reps >= 0) {
+        for(let i = 0; i < reps; i++) {
+            newString += string;
+        }
+    } else {
+        newString += 'ERROR';
+    }
+    
+    return newString;
 };
 
 // Do not edit below this line
