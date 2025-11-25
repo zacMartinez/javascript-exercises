e4a07eadd91a2844bcd11bff1a59814c851faecf
Zachary Martinez <zach.martinez@protonmail.com>
2025-11-24 18:57:01 -0800

    Solve exercise 09

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    const min = Math.min(a, b);
+    const max = a === min ? b : a;
 
+    if (Number.isInteger(min) 
+        && Number.isInteger(max)
+        && min >= 0
+        && max >= 0) {
+        let sum = 0;
+        for (let i = min; i <= max; i++) {
+            sum += i;
+        }
+
+        return sum;
+    } else {
+        return 'ERROR';
+    }
 };
 
 // Do not edit below this line
