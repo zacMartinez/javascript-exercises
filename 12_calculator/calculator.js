3c4efe98a2611f20a71199cb0b55cc54f10fa14a
Zachary Martinez <zach.martinez@protonmail.com>
2025-12-05 08:56:16 -0800

    Solve exercise 12

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(addends) {
+	return addends.reduce((sum, addend) => {return sum + addend}, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(factors) {
+  return factors.reduce((multiple, factor) => multiple * factor, 1);
 };
 
-const power = function() {
-	
+const power = function(b, n) {
+  return multiply(Array(n).fill(b));
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  if (n > 0) {
+    return multiply([...Array(n).keys().map(i => ++i)]); 
+  } else {
+    return 1;
+  }
 };
 
 // Do not edit below this line
