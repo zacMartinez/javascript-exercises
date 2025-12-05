6e0a0b7d3252f9ec66696fe237a59ccb4dc00597
Zachary Martinez <zach.martinez@protonmail.com>
2025-12-05 11:42:45 -0800

    Solve exercise 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const forward = [...string].filter(char => /\w+/.test(char))
+                               .join('')
+                               .toLowerCase();
+    const backward = [...forward].reverse().join('');
 
+    return forward === backward ? true : false;
 };
 
 // Do not edit below this line
