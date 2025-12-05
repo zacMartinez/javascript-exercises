e42abffa022a2ce8bd40a22eae1a08129075f1aa
Zachary Martinez <zach.martinez@protonmail.com>
2025-12-05 14:58:35 -0800

    Solve exercise 16, I'm proud of this one

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const thisYear = new Date().getFullYear();
 
+    return people.reduce((oldest, person) => {
+        const oldestAge = (oldest.yearOfDeath || thisYear) - oldest.yearOfBirth;
+        const currentAge = (person.yearOfDeath || thisYear) - person.yearOfBirth; 
+
+        if (currentAge > oldestAge) oldest = person;
+        return oldest;
+    },{yearOfBirth: thisYear, yearOfDeath: thisYear});
 };
 
 // Do not edit below this line
