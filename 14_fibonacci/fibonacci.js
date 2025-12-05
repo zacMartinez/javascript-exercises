21adb7c1d94591b52bb8a156aa4377b575be39cc
Zachary Martinez <zach.martinez@protonmail.com>
2025-12-05 14:39:58 -0800

    Solve exercise 14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if (n < 0) return 'OOPS';
+    
+    const sequence = [0,1];
+    for (let i = 2; i <= n; i++) {
+        sequence.push(sequence[i-1] + sequence[i-2]);
+    }
+    return sequence[n];
 };
 
 // Do not edit below this line
